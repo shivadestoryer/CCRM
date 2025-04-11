2d4274ad884a96d708da10d1477bab596511d2a4
shivadestoryer <shvmprasad@gmai.com>
2025-04-11 22:55:56 +0530

    Initial commit

---
+++ CCRM/src/main/java/com/sor/CCRM/CcrmApplication.java
@@ -0,0 +1,13 @@
+package com.sor.CCRM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcrmApplication.class, args);
+	}
+
+}
---
+++ CCRM/src/test/java/com/sor/CCRM/CcrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sor.CCRM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
